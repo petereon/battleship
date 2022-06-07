d5231e70c975014850a7311a927f4b109bb5f8b7
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2022-06-07 09:52:00 +0100

    chore: add mocker for the format table

--- test/util_test.py
+++ test/util_test.py
@@ -24,7 +24,28 @@ def game_state():
     }
 
 
+def mock_format_table():
+    table = Table(title="board_title")
+    table.add_column("", justify="right", style="cyan", no_wrap=True)
+    table.add_column("A", justify="right", style="cyan", no_wrap=True)
+    table.add_column("B", justify="right", style="cyan", no_wrap=True)
+    table.add_column("C", justify="right", style="cyan", no_wrap=True)
+    table.add_column("D", justify="right", style="cyan", no_wrap=True)
+    table.add_column("E", justify="right", style="cyan", no_wrap=True)
+    table.add_column("F", justify="right", style="cyan", no_wrap=True)
+    table.add_column("G", justify="right", style="cyan", no_wrap=True)
+    table.add_column("H", justify="right", style="cyan", no_wrap=True)
+    table.add_column("I", justify="right", style="cyan", no_wrap=True)
+    table.add_column("J", justify="right", style="cyan", no_wrap=True)
+
+    for count, data in enumerate(np.zeros((10, 10), dtype=int), start=1):
+        table.add_row(str(count), *list(map(str, data)))
+
+    return table
+
+
 def describe_get_formatter():
+    @patch("minesweeper.cli.util.format_table", lambda *args: mock_format_table())
     def test_get_console_player1_own_board_contents(game_state):
         formatter = util.get_formatter("console")
         tables: List[Table] = formatter(game_state)
@@ -35,6 +56,7 @@ def describe_get_formatter():
             expected_table.add_row(str(i + 1), *list(map(str, rows)))
         assert table_player1_own_board.rows == expected_table.rows
 
+    @patch("minesweeper.cli.util.format_table", lambda *args: mock_format_table())
     def test_get_console_player1_own_board_headers(game_state):
         formatter = util.get_formatter("console")
         tables: List[Table] = formatter(game_state)
@@ -54,6 +76,7 @@ def describe_get_formatter():
             "J",
         ]
 
+    @patch("minesweeper.cli.util.format_table", lambda *args: mock_format_table())
     def test_get_console_player1_shots_board_contents(game_state):
         formatter = util.get_formatter("console")
         tables: List[Table] = formatter(game_state)
@@ -64,6 +87,7 @@ def describe_get_formatter():
             expected_table.add_row(str(i + 1), *list(map(str, rows)))
         assert table_player1_shots_board.rows == expected_table.rows
 
+    @patch("minesweeper.cli.util.format_table", lambda *args: mock_format_table())
     def test_get_console_player2_own_board_contents(game_state):
         formatter = util.get_formatter("console")
         tables: List[Table] = formatter(game_state)
@@ -74,6 +98,7 @@ def describe_get_formatter():
             expected_table.add_row(str(i + 1), *list(map(str, rows)))
         assert table_player1_own_board.rows == expected_table.rows
 
+    @patch("minesweeper.cli.util.format_table", lambda *args: mock_format_table())
     def test_get_console_player2_shots_board_contents(game_state):
         formatter = util.get_formatter("console")
         tables: List[Table] = formatter(game_state)
@@ -95,7 +120,7 @@ def describe_format_table():
         board = np.zeros((10, 10), dtype=int)
         table: Table = util.format_table(board, "Test Player's Board")
 
-        table.title = "Test Player's Board"
+        assert table.title == "Test Player's Board"
 
     def test_table_x_axis():
         board = np.zeros((10, 10), dtype=int)
