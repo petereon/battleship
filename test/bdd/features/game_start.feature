9d7f085d9f89bf19531503d9d7f62e2aabda53bf
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2022-06-23 09:17:40 +0100

    refactor: change the steps for UAT2.1 into re-usable code

--- test/bdd/features/game_start.feature
+++ test/bdd/features/game_start.feature
@@ -2,6 +2,15 @@ Feature: Game Intialization
     Scenario: Game Unit Creation
         Given a new game
         When the game initialises
-        Then the two game units are set up with grids
-        And each grid is empty
-        And each grid is 10 x 10
+        Then the player 1 is set up with the ocean grid
+        And the player 1 is set up with the target grid
+        And the player 2 is set up with the ocean grid
+        And the player 2 is set up with the target grid
+        And the ocean grid for player 1 is empty
+        And the target grid for player 1 is empty
+        And the ocean grid for player 2 is empty
+        And the target grid for player 2 is empty
+        And the ocean grid for player 1 is 10 x 10
+        And the target grid for player 1 is 10 x 10
+        And the ocean grid for player 2 is 10 x 10
+        And the target grid for player 2 is 10 x 10
--- test/bdd/steps/game_start_test_steps.py
+++ test/bdd/steps/game_start_test_steps.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pytest_bdd import given, scenarios, then, when
+from pytest_bdd import given, parsers, scenarios, then, when
 
 from battleship.logic import Game, Grid
 
@@ -16,25 +16,24 @@ def when_the_game_initialises():
     return Game()
 
 
-@then("the two game units are set up with grids")
-def then_game_units_are_set_up(game):
-    assert isinstance(game.player1.ocean_grid, Grid)
-    assert isinstance(game.player1.target_grid, Grid)
-    assert isinstance(game.player2.ocean_grid, Grid)
-    assert isinstance(game.player2.target_grid, Grid)
+@then(parsers.parse("the player {player} is set up with the {grid} grid"))
+def then_the_player_is_set_up_with_grid(game, player, grid):
+    assert isinstance(
+        game.__getattribute__(f"player{player}").__getattribute__(f"{grid}_grid"),
+        Grid,
+    )
 
 
-@then("each grid is empty")
-def then_each_grid_is_empty(game):
-    assert np.all(game.player1.ocean_grid.matrix == 0)
-    assert np.all(game.player1.target_grid.matrix == 0)
-    assert np.all(game.player2.ocean_grid.matrix == 0)
-    assert np.all(game.player2.target_grid.matrix == 0)
+@then(parsers.parse("the {grid} grid for player {player} is empty"))
+def then_the_grid_is_empty(game, grid, player):
+    assert np.all(
+        game.__getattribute__(f"player{player}").__getattribute__(f"{grid}_grid").matrix == 0,
+    )
 
 
-@then("each grid is 10 x 10")
-def then_each_grid_is_10_x_10(game):
-    assert game.player1.ocean_grid.matrix.shape == (10, 10)
-    assert game.player1.target_grid.matrix.shape == (10, 10)
-    assert game.player2.ocean_grid.matrix.shape == (10, 10)
-    assert game.player2.target_grid.matrix.shape == (10, 10)
+@then(parsers.parse("the {grid} grid for player {player} is 10 x 10"))
+def then_the_grid_is_10x10(game, grid, player):
+    assert np.all(
+        game.__getattribute__(f"player{player}").__getattribute__(f"{grid}_grid").matrix.shape
+        == (10, 10),
+    )
