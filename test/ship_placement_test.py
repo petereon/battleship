bb16e9173bcc3645d41c35ba4517951b2f3151e7
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2022-06-29 15:39:07 +0100

    feat(us3/uat3.1): 🔴 red

--- test/ship_placement_test.py
+++ test/ship_placement_test.py
@@ -23,15 +23,22 @@ def describe_placing_ship():
         player.place_vessel(Vessel.CARRIER.value, (("C", "5"), ("C", "9")))
         assert player.ocean_grid.matrix[column_mapping["C"]][row_mapping["9"]] == CARRIER
 
-    def place_horizontal_carrer_on_C5_C9_length_is_5_test(get_player):
+    def place_horizontal_carrier_on_C5_C9_length_is_5_test(get_player):
         player = get_player
         player.place_vessel(Vessel.CARRIER.value, (("C", "5"), ("C", "9")))
         indices = np.argwhere(player.ocean_grid.matrix == CARRIER)
         assert len(indices) == 5
 
-    def place_horizontal_carrer_on_B2_B6_shape_is_5_test(get_player):
+    def place_horizontal_carrier_on_B2_B6_shape_is_5_test(get_player):
         player = get_player
         player.place_vessel(Vessel.CARRIER.value, (("B", "2"), ("B", "6")))
         indices = np.argwhere(player.ocean_grid.matrix == CARRIER)
         expected = np.array([[1, 1], [1, 2], [1, 3], [1, 4], [1, 5]])
+        np.testing.assert_array_equal(indices, expected)
+
+    def place_vertical_carrier_on_C5_C9_shape_is_5_test(get_player):
+        player = get_player
+        player.place_vessel(Vessel.CARRIER.value, (("C", "5"), ("G", "5")))
+        indices = np.argwhere(player.ocean_grid.matrix == CARRIER)
+        expected = np.array([[2, 5], [3, 5], [4, 5], [5, 5], [6, 5]])
         np.testing.assert_array_equal(indices, expected)
