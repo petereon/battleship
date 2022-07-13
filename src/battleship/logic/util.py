11da22917216102267c5eeb6ea7eea8ea24508ce
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2022-07-13 14:25:17 +0100

    refactor(us3/uat3.6): 👩‍💻 remove all of the printing

--- src/battleship/logic/util.py
+++ src/battleship/logic/util.py
@@ -12,18 +12,9 @@ def is_straight_line(coordinates):
 def is_valid_length(coordinates, vessel_type):
     (start_column_idx, start_row_idx), (end_column_idx, end_row_idx) = coordinates
 
-    print(
-        "!!!!",
-        column_mapping[start_column_idx],
-        column_mapping[end_column_idx],
-        abs(column_mapping[start_column_idx] - column_mapping[end_column_idx]),
-        VesselLength[vessel_type.value].value - 1,
-    )
     if start_column_idx == end_column_idx:
-        print("GOT HERE")
         return abs(row_mapping[start_row_idx] - row_mapping[end_row_idx]) == VesselLength[vessel_type.value].value - 1
     elif start_row_idx == end_row_idx:
-        print("IN HERE")
         return abs(column_mapping[start_column_idx] - column_mapping[end_column_idx]) == VesselLength[vessel_type.value].value - 1
 
 
@@ -35,6 +26,5 @@ def is_position_on_board(coordinates):
         column_mapping[start_column_idx]
         column_mapping[end_column_idx]
         return True
-    except KeyError as e:
-        print("KeyError received", e)
+    except KeyError:
         return False
