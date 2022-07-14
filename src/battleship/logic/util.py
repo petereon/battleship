d33d8548de631952ee23852729bfb79b98c87db4
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2022-07-14 10:25:06 +0100

    refactor(us3/uat3.6): 👩‍💻 changed the resturn for the uncovered lines

--- src/battleship/logic/util.py
+++ src/battleship/logic/util.py
@@ -4,20 +4,22 @@ from battleship.logic.constants import VesselLength, column_mapping, row_mapping
 def __is_straight_line(coordinates) -> bool:
     (start_column_idx, start_row_idx), (end_column_idx, end_row_idx) = coordinates
 
+    result = False
     if start_column_idx == end_column_idx or start_row_idx == end_row_idx:
-        return True
-    return False
+        result = True
+    return result
 
 
 def __is_valid_length(coordinates, vessel_type) -> bool:
     (start_column_idx, start_row_idx), (end_column_idx, end_row_idx) = coordinates
 
+    result = False
     if start_column_idx == end_column_idx:
-        return abs(row_mapping[start_row_idx] - row_mapping[end_row_idx]) == VesselLength[vessel_type.value].value - 1
+        result = abs(row_mapping[start_row_idx] - row_mapping[end_row_idx]) == VesselLength[vessel_type.value].value - 1
     elif start_row_idx == end_row_idx:
-        return abs(column_mapping[start_column_idx] - column_mapping[end_column_idx]) == VesselLength[vessel_type.value].value - 1
+        result = abs(column_mapping[start_column_idx] - column_mapping[end_column_idx]) == VesselLength[vessel_type.value].value - 1
 
-    return False
+    return result
 
 
 def __is_position_on_board(coordinates) -> bool:
@@ -33,4 +35,4 @@ def __is_position_on_board(coordinates) -> bool:
 
 
 def is_valid_position_for_vessel(coordinates, vessel_type) -> bool:
-    return __is_position_on_board(coordinates) and __is_straight_line(coordinates) and __is_valid_length(coordinates, vessel_type)
+    return __is_position_on_board(coordinates) and __is_valid_length(coordinates, vessel_type) and __is_straight_line(coordinates)
