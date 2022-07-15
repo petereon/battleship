004cea1bfb485c80fe488a86bacc9cd9da353d23
Peter Vyboch <peter.vyboch@ibm.com>
2022-07-15 15:27:39 +0200

    feat(us4/uat4.1): 🟢 green

--- test/bdd/features/shots.feature
+++ test/bdd/features/shots.feature
@@ -2,11 +2,12 @@ Feature: Shots taken during the game
 
     Scenario Outline: The game player makes a move by choosing a target hole on their target grid
         Given my target grid
+        And a hole <taken_hole> is already taken
         When I choose the hole <hole> on my target grid
         Then I know if it's available <available>
 
         Examples:
-            | hole | available |
-            | A1   | true      |
-            | H8   | false     |
-            | B7   | true      |
+            | hole | taken_hole | available |
+            | A1   | B2         | true      |
+            | H8   | H8         | false     |
+            | B7   | A1         | true      |
--- test/bdd/steps/shots_test_steps.py
+++ test/bdd/steps/shots_test_steps.py
@@ -4,6 +4,7 @@ import pytest
 from pytest_bdd import given, parsers, scenarios, then, when
 
 from battleship.logic import Grid, Player
+from battleship.logic.constants import Peg, column_mapping, row_mapping
 
 scenarios("../features/shots.feature")
 
@@ -16,6 +17,12 @@ def player():
 @given("my target grid", target_fixture="target_grid")
 def given_my_target_grid():
     return Grid()
+
+
+@given(parsers.parse("a hole {hole} is already taken"))
+def given_the_hole_is_already_taken(hole, target_grid):
+    column, row = (column_mapping[hole[0]], row_mapping[hole[1]])
+    target_grid.matrix[column][row] = Peg.RED
 
 
 @when(parsers.parse("I choose the hole {hole} on my target grid"), target_fixture="is_available")
